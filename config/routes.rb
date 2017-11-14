745e51219d2b4e0862caa55fe5e7ef922c0ce4c8
SRJ007 <suraj.yadav1994@gmail.com>
2017-11-14 17:50:59 +0000

    change in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+   get '/', to: "welcome/index"
    root 'welcome#index'
+   
+   get "/welcome", to: "welcome/index"
+   
+   
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
