c640bde1d9b373b08fdce5d9d01ee54f554545fc
SRJ007 <suraj.yadav1994@gmail.com>
2017-11-21 17:00:03 +0000

    added spinner in form

--- app/assets/javascripts/stock.js
+++ app/assets/javascripts/stock.js
@@ -5,10 +5,23 @@ $(document).ready(function(){
     var init_from_lookup;
 
 init_from_lookup = function(){
+     $('#stock-lookup-form').on("ajax:before",function(event, data, error){
+        $('#spinner').show();
+    });
+    
+     $('#stock-lookup-form').on("ajax:after",function(event, data, error){
+        $('#spinner').hide();
+    });
+    
     $('#stock-lookup-form').on("ajax:success",function(event, data, error){
         $('#stock-lookup').replaceWith(data);
         init_from_lookup();
         
+    })
+    
+     $('#stock-lookup-form').on("ajax:error",function(event, data, error){
+        $('#spinner').hide(); 
+        $('#stock-lookup-results').replaceWith("<p>Please enter valid symbol</p>");
     })
 }
     init_from_lookup();
